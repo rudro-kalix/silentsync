08367537c9003d496a0d85710ede97fc593085a0
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-03-26 12:45:50 +0600

    2026-03-26_12:45

---
+++ android/app/src/main/java/com/kalix/silent/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kalix.silent;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kalix.silent',
+  appName: 'Smart Mode',
+  webDir: 'dist'
+};
+
+export default config;
